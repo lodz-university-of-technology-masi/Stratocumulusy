217611810cbbfd9ff509e87db787f5202a7e0c91
Przemek <216827@edu.p.lodz.pl>
2019-12-15 16:58:30 +0100

    usuniecie questionClass1

--- frontend/src/containers/Recruiter.js
+++ frontend/src/containers/Recruiter.js
@@ -4,12 +4,17 @@ import { NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 
+let globalQuestions = [];
+let globalNumber = 0;
+
+
 class Recruiter extends Component {
   constructor(props) {
     super(props);
     this.state = {
        show: false,
-       questionType: 1
+       questionType: 3,
+       question: ''
       };
 
     this.showDiv = this.showDiv.bind(this)
@@ -27,18 +32,34 @@ class Recruiter extends Component {
 
   showDiv = () => {
     const { show } = this.state;
-    this.setState({ show: true })
+    this.setState({ show: true });
+
+
   }
 
+
   render() {
-    return (
+
+      return (
       <div className="Recruiter">
         <div className="lander">
 
           <div>
             <label>Enter the test title</label>
             <input type="text" name="title" />
-            <button>Save test</button>
+            <button onClick={function () {
+                const response = fetch('https://k8mmeonpwf.execute-api.us-east-1.amazonaws.com/prod/emptytests', {
+                    method: 'POST',
+                    body: JSON.stringify({"question": {globalQuestions}}),
+                    headers: {
+                        "Content-type": "application/json; charset=UTF-8"
+                    }
+                });
+
+                return false;
+            }}>Save test</button>
+            <br /><br />
+              <label>Number of question: {globalNumber}</label>
             <br /><br />
           </div>
           <div>
@@ -50,58 +71,89 @@ class Recruiter extends Component {
             </select>
             <br />
             <button onClick={this.showDiv}>Add question</button>
-            {this.state.show && <Question1 questionType={this.state.questionType}/>}
-            
-          </div>
+              {this.state.show && this.state.questionType == 3 ?
+                  <>
+                      <label>Question no. 1</label>
+                      < br/> < br />
+                      < label > Enter content of the question</label>
+                      <br />
+                      <textarea id="question" rows="5" cols="100"/>
+                      <br /><br />
 
+                      <label>Enter answer</label>
+                      <br/>
+                      <input type="text" name="1answer" />
+                      <br/><br/>
+                      <button onClick={function () {
+                          globalNumber++;
+                          globalQuestions.push(document.getElementById("question").innerText);
+                          return false;
+                      }}>Save question</button>
+                  </> : null
+              }
+            {/*{this.state.show && this.state.questionType==1 ?*/}
+            {/*  <>*/}
+            {/*      <label>Question no. 1</label>*/}
+            {/*      <br /><br />*/}
+            {/*      <label>Enter content of the question</label>*/}
+            {/*      <br />*/}
+            {/*      <textarea id="question" rows="5" cols="100"/>*/}
+            {/*      <br /><br />*/}
+
+            {/*          <label>Enter 1st answer</label>*/}
+            {/*          <br/>*/}
+            {/*          <input type="text" name="1answer" />*/}
+            {/*          <br/><br/>*/}
+            {/*          <label>Enter 2nd answer</label>*/}
+            {/*          <br/>*/}
+            {/*          <input type="text" name="2answer" />*/}
+            {/*          <br/><br/>*/}
+            {/*          <label>Enter 3rd answer</label>*/}
+            {/*          <br/>*/}
+            {/*          <input type="text" name="3answer" />*/}
+            {/*          <br/><br/>*/}
+            {/*          <label>Enter 4th answer</label>*/}
+            {/*          <br/>*/}
+            {/*          <input type="text" name="4answer" />*/}
+            {/*          <br/><br/>*/}
+            {/*          <button onClick={function () {*/}
+            {/*              globalNumber++;*/}
+            {/*              globalQuestions.push(document.getElementById("question").innerText);*/}
+            {/*              return false;*/}
+            {/*          }}>Save question</button>*/}
+            {/*      </> : null*/}
+            {/*      }*/}
+
+            {/*      {this.state.show && this.state.questionType == 2 ?*/}
+            {/*          <>*/}
+            {/*          <label>Question no. 1</label>*/}
+            {/*          < br/> < br />*/}
+            {/*          < label > Enter content of the question</label>*/}
+            {/*          <br />*/}
+            {/*          <textarea id="question" rows="5" cols="100"/>*/}
+            {/*          <br /><br />*/}
+
+            {/*          <label>Enter answer</label>*/}
+            {/*          <br/>*/}
+            {/*          <input type="text" name="1answer" />*/}
+            {/*              <br/><br/>*/}
+            {/*              <button onClick={function () {*/}
+            {/*                  globalNumber++;*/}
+            {/*                  globalQuestions.push(document.getElementById("question").innerText);*/}
+            {/*                  return false;*/}
+            {/*              }}>Save question</button>*/}
+            {/*        </> : null*/}
+            {/*      }*/}
+
+          </div>
+          </div>
         </div>
 
-      </div>
+
     );
   }
 }
 
-class Question1 extends Component{
-  render() {
-    return (
-      <div>
-        <label>Question no. 1</label>
-        <br /><br />
-        <label>Enter content of the question</label>
-        <br />
-        <textarea rows="5" cols="100" />
-        <br /><br />
-        { this.props.questionType == 1 &&
-        <div>
-        <label>Enter 1st answer</label>
-        <br/>
-        <input type="text" name="1answer" />
-        <br/><br/>
-        <label>Enter 2nd answer</label>
-        <br/>
-        <input type="text" name="2answer" />
-        <br/><br/>
-        <label>Enter 3rd answer</label>
-        <br/>
-        <input type="text" name="3answer" />
-        <br/><br/>
-        <label>Enter 4th answer</label>
-        <br/>
-        <input type="text" name="4answer" />
-        </div>
-        }
-        { this.props.questionType == 3 &&
-        <div>
-        <label>Enter answer</label>
-        <br/>
-        <input type="text" name="1answer" />
-        </div>
-        }
-        <br/><br/>
-        <button>Save question</button>
-      </div>
-    )
-  }
-}
+
 
 export default Recruiter;
