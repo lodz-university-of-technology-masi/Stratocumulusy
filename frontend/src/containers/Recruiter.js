ad8d75e817d58d090b7e140d7181a70eb57ce936
Przemek <216827@edu.p.lodz.pl>
2019-12-16 09:07:04 +0100

    save testTittle, questionNumber, questionsTab

--- frontend/src/containers/Recruiter.js
+++ frontend/src/containers/Recruiter.js
@@ -1,41 +1,79 @@
 import React, { Component } from "react";
 import "./Recruiter.css";
-import { NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 
 
 let globalQuestions = [];
-let globalNumber = 0;
-
 
 class Recruiter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       show: false,
+        testTittle: '',
        questionType: 3,
+        numberOfQuestions: 0,
        question: ''
       };
 
-    this.showDiv = this.showDiv.bind(this)
+    this.handleQuestionType=this.handleQuestionType.bind(this);
+    this.handleQuestionTextChange=this.handleQuestionTextChange.bind(this);
+    this.handleTestTittleChange=this.handleTestTittleChange.bind(this);
+    this.saveOpenQuestion=this.saveOpenQuestion.bind(this);
+    this.saveTestToDynamoDB=this.saveTestToDynamoDB.bind(this);
+
   }
 
-  get qtype(){
-    return this.questionType;
-  }
-
-  handleQuestionType = event => {
+  handleQuestionType = (event)=> {
     this.setState({
       questionType: event.target.value
     })
   }
 
-  showDiv = () => {
-    const { show } = this.state;
-    this.setState({ show: true });
 
+    handleQuestionTextChange = (event) => {
+        this.setState({
+            question: event.target.value
+        })
+    };
 
-  }
+    handleTestTittleChange = (event) => {
+        this.setState({
+            testTittle: document.getElementById("testTitle").value
+        })
+    };
+
+    saveOpenQuestion = (event) => {
+        this.state.numberOfQuestions++;
+
+        globalQuestions.push(this.state.question);
+        this.setState({
+            question: ' '
+        });
+
+    }
+
+    saveTestToDynamoDB (event) {
+        // const questions = globalQuestions.map((val, ind) => {
+        //     return {"id": ind, "questionTitle": val, "questionContent": "----"}
+        // })
+
+        var testTittle = this.state.testTittle; // robie to bo takto jest undefined ....
+
+        const test = {
+            "testTitle": testTittle,
+            "numberOfQuestions": this.state.numberOfQuestions,
+            "questions": globalQuestions
+        }
+
+        const response = fetch('https://k8mmeonpwf.execute-api.us-east-1.amazonaws.com/prod/emptytests', {
+            dataType: "json",
+            method: 'POST',
+            body: JSON.stringify(test),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        return false;
+    }
 
 
   render() {
@@ -46,20 +84,10 @@ class Recruiter extends Component {
 
           <div>
             <label>Enter the test title</label>
-            <input type="text" name="title" />
-            <button onClick={function () {
-                const response = fetch('https://k8mmeonpwf.execute-api.us-east-1.amazonaws.com/prod/emptytests', {
-                    method: 'POST',
-                    body: JSON.stringify({"question": {globalQuestions}}),
-                    headers: {
-                        "Content-type": "application/json; charset=UTF-8"
-                    }
-                });
-
-                return false;
-            }}>Save test</button>
+            <input id="testTitle" type="text" value={this.state.testTittle} onChange={this.handleTestTittleChange}/>
+            <button onClick={this.saveTestToDynamoDB}>Save test</button>
             <br /><br />
-              <label>Number of question: {globalNumber}</label>
+              <label>Number of question: {this.state.numberOfQuestions}</label>
             <br /><br />
           </div>
           <div>
@@ -70,80 +98,56 @@ class Recruiter extends Component {
               <option value="3">Number</option>
             </select>
             <br />
-            <button onClick={this.showDiv}>Add question</button>
-              {this.state.show && this.state.questionType == 3 ?
+              {this.state.questionType === 3 ?
                   <>
-                      <label>Question no. 1</label>
-                      < br/> < br />
+                      <br />
                       < label > Enter content of the question</label>
                       <br />
-                      <textarea id="question" rows="5" cols="100"/>
+                      <textarea id="question" rows="5" cols="100" value={this.state.question} onChange={this.handleQuestionTextChange}/>
                       <br /><br />
 
                       <label>Enter answer</label>
                       <br/>
                       <input type="text" name="1answer" />
-                      <br/><br/>
-                      <button onClick={function () {
-                          globalNumber++;
-                          globalQuestions.push(document.getElementById("question").innerText);
-                          return false;
-                      }}>Save question</button>
                   </> : null
               }
-            {/*{this.state.show && this.state.questionType==1 ?*/}
-            {/*  <>*/}
-            {/*      <label>Question no. 1</label>*/}
-            {/*      <br /><br />*/}
-            {/*      <label>Enter content of the question</label>*/}
-            {/*      <br />*/}
-            {/*      <textarea id="question" rows="5" cols="100"/>*/}
-            {/*      <br /><br />*/}
+            {this.state.questionType===1 ?
+              <>
+                  <br />
+                  <label>Enter content of the question</label>
+                  <br />
+                  <textarea id="question" rows="5" cols="100"/>
+                  <br /><br />
 
-            {/*          <label>Enter 1st answer</label>*/}
-            {/*          <br/>*/}
-            {/*          <input type="text" name="1answer" />*/}
-            {/*          <br/><br/>*/}
-            {/*          <label>Enter 2nd answer</label>*/}
-            {/*          <br/>*/}
-            {/*          <input type="text" name="2answer" />*/}
-            {/*          <br/><br/>*/}
-            {/*          <label>Enter 3rd answer</label>*/}
-            {/*          <br/>*/}
-            {/*          <input type="text" name="3answer" />*/}
-            {/*          <br/><br/>*/}
-            {/*          <label>Enter 4th answer</label>*/}
-            {/*          <br/>*/}
-            {/*          <input type="text" name="4answer" />*/}
-            {/*          <br/><br/>*/}
-            {/*          <button onClick={function () {*/}
-            {/*              globalNumber++;*/}
-            {/*              globalQuestions.push(document.getElementById("question").innerText);*/}
-            {/*              return false;*/}
-            {/*          }}>Save question</button>*/}
-            {/*      </> : null*/}
-            {/*      }*/}
+                      <label>Enter 1st answer</label>
+                      <br/>
+                      <input type="text" name="1answer" />
+                      <br/><br/>
+                      <label>Enter 2nd answer</label>
+                      <br/>
+                      <input type="text" name="2answer" />
+                      <br/><br/>
+                      <label>Enter 3rd answer</label>
+                      <br/>
+                      <input type="text" name="3answer" />
+                      <br/><br/>
+                      <label>Enter 4th answer</label>
+                      <br/>
+                      <input type="text" name="4answer" />
+                  </> : null
+                  }
 
-            {/*      {this.state.show && this.state.questionType == 2 ?*/}
-            {/*          <>*/}
-            {/*          <label>Question no. 1</label>*/}
-            {/*          < br/> < br />*/}
-            {/*          < label > Enter content of the question</label>*/}
-            {/*          <br />*/}
-            {/*          <textarea id="question" rows="5" cols="100"/>*/}
-            {/*          <br /><br />*/}
+                  {this.state.questionType === 2 ?
+                      <>
+                          <br />
+                      < label > Enter content of the question</label>
+                      <br />
+                      <textarea id="question" rows="5" cols="100"/>
+                    </> : null
+                  }
 
-            {/*          <label>Enter answer</label>*/}
-            {/*          <br/>*/}
-            {/*          <input type="text" name="1answer" />*/}
-            {/*              <br/><br/>*/}
-            {/*              <button onClick={function () {*/}
-            {/*                  globalNumber++;*/}
-            {/*                  globalQuestions.push(document.getElementById("question").innerText);*/}
-            {/*                  return false;*/}
-            {/*              }}>Save question</button>*/}
-            {/*        </> : null*/}
-            {/*      }*/}
+              <br/><br/>
+              <button onClick={this.saveOpenQuestion}>Save question</button>
 
           </div>
           </div>
