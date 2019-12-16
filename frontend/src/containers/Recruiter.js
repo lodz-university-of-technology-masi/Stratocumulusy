08bb9a026380a72033a806a31f891dc28c790386
Przemek <216827@edu.p.lodz.pl>
2019-12-16 10:06:39 +0100

    Merge branch 'master' of C:\Users\Przemysław Łapiński\Desktop\stratocumulusy\Stratocumulusy with conflicts.

--- frontend/src/containers/Recruiter.js
+++ frontend/src/containers/Recruiter.js
@@ -1,44 +1,93 @@
 import React, { Component } from "react";
 import "./Recruiter.css";
-import { NavItem } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
 
+
+let globalQuestions = [];
 
 class Recruiter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-       show: false,
-       questionType: 1
+        testTittle: '',
+       questionType: 3,
+        numberOfQuestions: 0,
+       question: ''
       };
 
-    this.showDiv = this.showDiv.bind(this)
+    this.handleQuestionType=this.handleQuestionType.bind(this);
+    this.handleQuestionTextChange=this.handleQuestionTextChange.bind(this);
+    this.handleTestTittleChange=this.handleTestTittleChange.bind(this);
+    this.saveOpenQuestion=this.saveOpenQuestion.bind(this);
+    this.saveTestToDynamoDB=this.saveTestToDynamoDB.bind(this);
+
   }
 
-  get qtype(){
-    return this.questionType;
-  }
-
-  handleQuestionType = event => {
+  handleQuestionType = (event)=> {
     this.setState({
       questionType: event.target.value
     })
   }
 
-  showDiv = () => {
-    const { show } = this.state;
-    this.setState({ show: true })
-  }
+
+    handleQuestionTextChange = (event) => {
+        this.setState({
+            question: event.target.value
+        })
+    };
+
+    handleTestTittleChange = (event) => {
+        this.setState({
+            testTittle: document.getElementById("testTitle").value
+        })
+    };
+
+    saveOpenQuestion = (event) => {
+        this.state.numberOfQuestions++;
+
+        globalQuestions.push(this.state.question);
+        this.setState({
+            question: ' '
+        });
+
+    }
+
+    saveTestToDynamoDB (event) {
+        // const questions = globalQuestions.map((val, ind) => {
+        //     return {"id": ind, "questionTitle": val, "questionContent": "----"}
+        // })
+
+        var testTittle = this.state.testTittle; // robie to bo takto jest undefined ....
+
+        const test = {
+            "testTittle": testTittle,
+            "numberOfQuestions": this.state.numberOfQuestions,
+            "questions": globalQuestions
+        }
+
+        const response = fetch('https://nbbmfshcof.execute-api.us-east-1.amazonaws.com/test/emptytest', {
+            dataType: "json",
+            method: 'POST',
+            body: JSON.stringify(test),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        });
+        return false;
+    }
+
 
   render() {
-    return (
+
+      return (
       <div className="Recruiter">
         <div className="lander">
 
           <div>
             <label>Enter the test title</label>
-            <input type="text" name="title" />
-            <button>Save test</button>
+            <input id="testTitle" type="text" value={this.state.testTittle} onChange={this.handleTestTittleChange}/>
+            <button onClick={this.saveTestToDynamoDB}>Save test</button>
+            <br /><br />
+              <label>Number of question: {this.state.numberOfQuestions}</label>
             <br /><br />
           </div>
           <div>
@@ -49,59 +98,66 @@ class Recruiter extends Component {
               <option value="3">Number</option>
             </select>
             <br />
-            <button onClick={this.showDiv}>Add question</button>
-            {this.state.show && <Question1 questionType={this.state.questionType}/>}
-            
-          </div>
+              {this.state.questionType === 3 ?
+                  <>
+                      <br />
+                      < label > Enter content of the question</label>
+                      <br />
+                      <textarea id="question" rows="5" cols="100" value={this.state.question} onChange={this.handleQuestionTextChange}/>
+                      <br /><br />
 
+                      <label>Enter answer</label>
+                      <br/>
+                      <input type="text" name="1answer" />
+                  </> : null
+              }
+            {this.state.questionType===1 ?
+              <>
+                  <br />
+                  <label>Enter content of the question</label>
+                  <br />
+                  <textarea id="question" rows="5" cols="100"/>
+                  <br /><br />
+
+                      <label>Enter 1st answer</label>
+                      <br/>
+                      <input type="text" name="1answer" />
+                      <br/><br/>
+                      <label>Enter 2nd answer</label>
+                      <br/>
+                      <input type="text" name="2answer" />
+                      <br/><br/>
+                      <label>Enter 3rd answer</label>
+                      <br/>
+                      <input type="text" name="3answer" />
+                      <br/><br/>
+                      <label>Enter 4th answer</label>
+                      <br/>
+                      <input type="text" name="4answer" />
+                  </> : null
+                  }
+
+                  {this.state.questionType === 2 ?
+                      <>
+                          <br />
+                      < label > Enter content of the question</label>
+                      <br />
+                      <textarea id="question" rows="5" cols="100"/>
+                    </> : null
+                  }
+
+              <br/><br/>
+              <button onClick={this.saveOpenQuestion}>Save question</button>
+
+          </div>
+          </div>
         </div>
 
-      </div>
+
     );
   }
 }
 
-class Question1 extends Component{
-  render() {
-    return (
-      <div>
-        <label>Question no. 1</label>
-        <br /><br />
-        <label>Enter content of the question</label>
-        <br />
-        <textarea rows="5" cols="100" />
-        <br /><br />
-        { this.props.questionType == 1 &&
-        <div>
-        <label>Enter 1st answer</label>
-        <br/>
-        <input type="text" name="1answer" />
-        <br/><br/>
-        <label>Enter 2nd answer</label>
-        <br/>
-        <input type="text" name="2answer" />
-        <br/><br/>
-        <label>Enter 3rd answer</label>
-        <br/>
-        <input type="text" name="3answer" />
-        <br/><br/>
-        <label>Enter 4th answer</label>
-        <br/>
-        <input type="text" name="4answer" />
-        </div>
-        }
-        { this.props.questionType == 3 &&
-        <div>
-        <label>Enter answer</label>
-        <br/>
-        <input type="text" name="1answer" />
-        </div>
-        }
-        <br/><br/>
-        <button>Save question</button>
-      </div>
-    )
-  }
-}
+
 
 export default Recruiter;
