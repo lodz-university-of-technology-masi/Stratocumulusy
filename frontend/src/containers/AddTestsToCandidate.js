9d57a30de1363f25540441c8ef80bc7ba6c1fcbd
Przemek <216827@edu.p.lodz.pl>
2019-12-30 18:38:37 +0100

    wyswietlanie list

--- frontend/src/containers/AddTestsToCandidate.js
+++ frontend/src/containers/AddTestsToCandidate.js
@@ -13,9 +13,15 @@ class AddTestToCandidate extends Component {
             selectedCandidate: '',
             isCandidateSelected: false,
             candidateList: [],
+            availableTests: [],
+            selectedTests: [],
         };
+
         this.selectCandidate = this.selectCandidate.bind(this);
         this.cancel = this.cancel.bind(this);
+        this.save = this.save.bind(this);
+        this.addTest = this.addTest.bind(this);
+        this.removeTest = this.removeTest.bind(this);
     }
 
     componentDidMount = () => {
@@ -47,17 +53,62 @@ class AddTestToCandidate extends Component {
         this.setState({
             selectedCandidate: '',
             isCandidateSelected: false,
+            availableTests: [],
+            selectedTests: [],
+        })
+    };
+    save = (event) => {
+        this.setState({
+            selectedCandidate: '',
+            isCandidateSelected: false,
+            availableTests: [],
+            selectedTests: [],
         })
     };
 
     selectCandidate = (event) => {
         let nameCandidate = event.target.getAttribute('nameCandidate');
         console.log("nameCandidate  : " +nameCandidate);
+        // pobranie testow
+        let testy = null;
+        fetch('https://nbbmfshcof.execute-api.us-east-1.amazonaws.com/test/emptytest')
+            .then((response)=>{return response.json()})
+            .then((data)=>{
+                console.log("testy: "+data.toSource());
+                testy = data["Items"].map(n => n.testTittle);
+                console.log("testy w metodzie: "+testy);
+            })
+            .finally(() =>
+            {
+                console.log("testy po metodzie: " + testy);
+                // pobranie testow jakie ma juz kandydat
+                this.setState({
+                    selectedCandidate: nameCandidate,
+                    isCandidateSelected: true,
+                    availableTests: testy,
+                })
+            });
+
+    };
+    addTest = (event) => {
+        let test = event.target.getAttribute('test');
+        console.log("test  : " +test);
+        const selectedTests = this.state.selectedTests.slice();
+        selectedTests.push(test);
+                this.setState({
+                    selectedTests: selectedTests,
+                })
+    };
+    removeTest = (event) => {
+        let testToRemove = event.target.getAttribute('test');
+        console.log("test  : " +testToRemove);
+        let selectedTests = this.state.selectedTests.slice();
+        selectedTests = selectedTests.filter(test => test!=testToRemove);
         this.setState({
-            selectedCandidate: nameCandidate,
-            isCandidateSelected: true,
+            selectedTests: selectedTests,
         })
     };
+
 
 
 
@@ -68,13 +119,21 @@ class AddTestToCandidate extends Component {
         const selectedCandidate = this.state.selectedCandidate;
         const isCandidateSelected = this.state.isCandidateSelected;
         const candidateList = this.state.candidateList;
+        const availableTests = this.state.availableTests;
+        const selectedTests = this.state.selectedTests;
+
         console.log("candidateList  : " +this.state.candidateList);
+        console.log("selectedTests: "+selectedTests);
 
         return (
             <div className="AddTestToCandidate">
                 <div className="lander">
                     <label>Selected candidate: {selectedCandidate} </label>
                     <button onClick={this.cancel}>Cancel</button>
+                    {isCandidateSelected == true ?
+                        <>
+                            <button onClick={this.save}>Save</button>
+                        </> : null}
                     <br/>
 
                     {isCandidateSelected == false ?
@@ -87,13 +146,32 @@ class AddTestToCandidate extends Component {
                             )
 
                         )}
-
-
-
-
-
                     </>
-                    :null
+                    :
+                        <>
+                        <div className="AvailableTest">
+                            <h1>Available tests:</h1>
+                            {availableTests.map(c =>
+                                ( <div>
+                                        <h>Test title {c} </h>
+                                        <button onClick={this.addTest} test={c}>Add test</button>
+                                    </div>
+                                )
+
+                            )}
+                        </div>
+                            <div className="SelectedTest">
+                                <h1>Selected tests:</h1>
+                                {selectedTests.map(c =>
+                                    ( <div>
+                                            <h>Test title {c}</h>
+                                            <button onClick={this.removeTest} test={c}>Remove test</button>
+                                        </div>
+                                    )
+
+                                )}
+                            </div>
+                        </>
                     }
 
 
