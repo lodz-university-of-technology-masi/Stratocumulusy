c1afedc5153d5134a0244da575541b4db1441888
PawelBialek <216723@edu.p.lodz.pl>
2020-01-05 15:05:36 +0100

    Proba implementacji edycji testow - bez credentials

--- frontend/src/containers/CognitoUsers.js
+++ frontend/src/containers/CognitoUsers.js
@@ -11,6 +11,6 @@ export const params = {
 export const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({
     apiVersion: '2016-04-18',
     region: config.cognito.REGION,
-    credentials: new AWS.Credentials('ASIAYAN7EXI7KIKCHNWW', 'WhkBJKq9DAZ9s5T4/LGjdd13iEt5/pTZThVAbLDP'
-        , 'FwoGZXIvYXdzEOX//////////wEaDI6xCokaMRnkf2m2kyLDAWrlC+qHCmKkp5haL98o5n8hGTC6ycPccISE0L2PXGzyJzNNKUmnnuItMthSbxHYKeCek89QGnYopCkYdNVWQFmkpzCh/1rXbCUnryrfT3ygwUZeMBLPtyx2bLXdynKAqV1r1l1VJsEd5n/XSHQdniFDgBdN7X+hkB6YGiLVJiDI3009h6kGYx8XJrMGa3MYjsAbhkAgctOsIhuMcxitfWQh5Sql0evP8DWBOpSnGc5XkMKXuuPNEYZofNsXytx43j8sRijxrr7wBTItRXPskn2xi/TznQfyrKtrIVnSr19sX9GEpQYJjQMnW/jIDY0diWzYGRSSW587')
+    credentials: new AWS.Credentials('', ''
+        , '')
 });
